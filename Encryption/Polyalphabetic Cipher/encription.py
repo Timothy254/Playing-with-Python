b2a5f1081aecc19f39a5aecaad282eb4700deda6
Timothy254 <71636232+Timothy254@users.noreply.github.com>
2021-06-21 15:35:02 +0530

    Create encription.py

---
+++ Encryption/Polyalphabetic Cipher/encription.py
@@ -0,0 +1,26 @@
+def generateKey(string, key):
+    key = list(key)
+    if len(string) == len(key):
+        return(key)
+    else:
+        for i in range(len(string) -
+                       len(key)):
+            key.append(key[i % len(key)])
+    return("" . join(key))
+
+
+def cipherText(string, key):
+    cipher_text = []
+    for i in range(len(string)):
+        x = (ord(string[i]) +
+             ord(key[i])) % 26
+        x += ord('A')
+        cipher_text.append(chr(x))
+    return("" . join(cipher_text))
+
+
+string = input("Enter the plain text: ")
+keyword = input("Enter the key: ")
+key = generateKey(string, keyword)
+cipher_text = cipherText(string,key)
+print("Ciphertext :", cipher_text)
