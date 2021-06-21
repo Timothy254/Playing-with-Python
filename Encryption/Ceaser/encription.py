f352c93827db1a74371a5d0ec2aeee3163dbb37c
Timothy254 <71636232+Timothy254@users.noreply.github.com>
2021-06-21 15:30:53 +0530

    Create encription.py

---
+++ Encryption/Ceaser/encription.py
@@ -0,0 +1,18 @@
+def encrypt(string, shift):
+
+  cipher = ''
+  for char in string:
+    if char == ' ':
+      cipher = cipher + char
+    elif  char.isupper():
+      cipher = cipher + chr((ord(char) + shift - 65) % 26 + 65)
+    else:
+      cipher = cipher + chr((ord(char) + shift - 97) % 26 + 97)
+
+  return cipher
+#check the above function
+
+pt= input("enter string: ")
+s = int(input("enter shift number: "))
+print("Original string: ", pt)
+print("After encryption: ", encrypt(pt, s))
